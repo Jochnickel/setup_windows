fae92b2c6e0c97207f2b8466964880a06fd7b9e2
Joachim <47549862+Schokokex@users.noreply.github.com>
2020-11-17 11:42:26 +0100

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -1,1 +1,11 @@
 Get-AppxPackage Microsoft.YourPhone -AllUsers | Remove-AppxPackage
+net user Administrator /active:yes
+net user Administrator *
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:Containers-DisposableClientVM /all /norestart
+dism.exe /online /enable-feature /featurename:NetFx3 /all /norestart
+wsl --set-default-version 2
+
+
+echo enable incoming remote
+netplwiz
