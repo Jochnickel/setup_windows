1c5f040f11fd91d54e9ebcadffc45dd85eff90ff
Joachim <47549862+Schokokex@users.noreply.github.com>
2020-11-18 00:18:25 +0100

    Update makeResetStick.ps1

--- makeResetStick.ps1
+++ makeResetStick.ps1
@@ -12,44 +12,64 @@ $isoDriveLetter = (Mount-DiskImage $isoFile -PassThru | Get-Volume).DriveLetter
 If ($isoDriveLetter -eq ""){echo couldnt mount image; exit}
 If ($isoDriveLetter -eq $null){echo couldnt mount image; exit}
 
-#copy to stick
-Start-Job -ScriptBlock {robocopy $args} -ArgumentList $isoDriveLetter':\', $thumbDrive, /S,  /XF, install.esd
-
-Get-Job
 #  move to temp dir
 New-TemporaryFile | %{ rm $_ ; mkdir $_ ; cd $_}
 
-Get-Job
-# unpack wim
-mkdir wim
-Dism.exe /Export-Image /SourceImageFile:$isoDriveLetter":\sources\install.esd" /SourceName:$version /DestinationImageFile:.\wim\install.wim /Compress:Max /CheckIntegrity
 
-Get-Job
-#TODO mount image
-mkdir offline
-Mount-WindowsImage -Path .\offline\ -ImagePath .\wim\install.wim -Name $version
 
-Get-Job
-#TODO export drivers
-mkdir drivers
-Export-WindowsDriver -Online -Destination .\drivers
+#copy iso to stick
+$jobFileCopy = Start-Job -ScriptBlock {
+  robocopy $args
+} -ArgumentList $isoDriveLetter':\', $thumbDrive, /S,  /XF, install.esd
 
-Get-Job
-#TODO add drivers to image
-Add-WindowsDriver -Path .\offline -Driver .\drivers -Recurse
+#export drivers
+$jobExportDrivers = Start-Job -ScriptBlock {
+  mkdir drivers
+  Export-WindowsDriver -Online -Destination .\drivers
+}
 
-Get-Job
-# remove YourPhone app
-Remove-AppxProvisionedPackage -PackageName Microsoft.YourPhone -Path .\offline
-Enable-WindowsOptionalFeature -Path .\offline -FeatureName "NetFx3" -All
+#iso/esd -> wim
+$jobUnpackWim = Start-Job -ScriptBlock {
+  mkdir wim
+  Dism.exe $args
+} -ArgumentList /Export-Image, /SourceImageFile:$isoDriveLetter":\sources\install.esd", /SourceName:$version, /DestinationImageFile:.\wim\install.wim, /Compress:Max, /CheckIntegrity
 
-Get-Job
-# set product key
-$productKey = (Get-WmiObject -query 'select * from SoftwareLicensingService').OA3xOriginalProductKey
-Set-WindowsProductKey -Path .\offline -ProductKey $productKey
+# wim mount
+$jobMountImage = Start-Job -ScriptBlock {
+  $args[0] | Wait-Job
+  mkdir offline
+  Mount-WindowsImage -Path .\offline\ -ImagePath .\wim\install.wim -Name $args[1]
+} -ArgumentList $jobUnpackWim, $version
+
+
+#import drivers
+$jobAddDrivers = Start-Job -ScriptBlock {
+  $args[0] | Wait-Job
+  $args[1] | Wait-Job
+  Add-WindowsDriver -Path .\offline -Driver .\drivers -Recurse
+} -ArgumentList $jobExportDrivers, $jobMountImage
+
+
+# remove YourPhone app from mounted
+$jobCustomizeMount = Start-Job -ScriptBlock {
+  $args[0] | Wait-Job
+  
+  Remove-AppxProvisionedPackage -PackageName Microsoft.YourPhone -Path .\offline
+  Enable-WindowsOptionalFeature -Path .\offline -FeatureName "NetFx3" -All
+  
+  # set product key
+  $productKey = (Get-WmiObject -query 'select * from SoftwareLicensingService').OA3xOriginalProductKey
+  Set-WindowsProductKey -Path .\offline -ProductKey $productKey
+  
+} -ArgumentList $jobMountImage
+
+
+$jobMountImage = Start-Job -ScriptBlock {
+  $args[0] | Wait-Job
+  Dismount-WindowsImage -Path .\offline -Save
+} -ArgumentList $jobCustomizeMount
 
 Get-Job
 #pack
-Dismount-WindowsImage -Path .\offline -Save
 
 
