3a58c0423a2706d3b5ddef809ae2ba7fd1080c10
Joachim <47549862+Schokokex@users.noreply.github.com>
2020-11-18 01:57:06 +0100

    Update makeResetStick.ps1

--- makeResetStick.ps1
+++ makeResetStick.ps1
@@ -1,77 +1,92 @@
-#needs admin rights
+# #needs admin rights
+# #error "The file install.wim is too big for the stick
 
-$isoFile = $args[0]
-$thumbDrive = $args[1]
+# $isoFile = "D:\Downloads\Windows.iso"
+# $thumbDriveLetter = "E"
 
-$version = "Windows 10 Pro"
-
-#  mount iso
-$isoDriveLetter = (Mount-DiskImage $isoFile -PassThru | Get-Volume).DriveLetter
-
-#TODO check letter automtaically
-If ($isoDriveLetter -eq ""){echo couldnt mount image; exit}
-If ($isoDriveLetter -eq $null){echo couldnt mount image; exit}
-
-#  move to temp dir
-New-TemporaryFile | %{ rm $_ ; mkdir $_ ; cd $_}
+# $version = "Windows 10 Pro"
 
 
-
-#copy iso to stick
-$jobFileCopy = Start-Job -ScriptBlock {
-  robocopy $args
-} -ArgumentList $isoDriveLetter':\', $thumbDrive, /S,  /XF, install.esd
-
-#export drivers
-$jobExportDrivers = Start-Job -ScriptBlock {
-  mkdir drivers
-  Export-WindowsDriver -Online -Destination .\drivers
-}
-
-#iso/esd -> wim
-$jobUnpackWim = Start-Job -ScriptBlock {
-  mkdir wim
-  Dism.exe $args
-} -ArgumentList /Export-Image, /SourceImageFile:$isoDriveLetter":\sources\install.esd", /SourceName:$version, /DestinationImageFile:.\wim\install.wim, /Compress:Max, /CheckIntegrity
-
-# wim mount
-$jobMountImage = Start-Job -ScriptBlock {
-  $args[0] | Wait-Job
-  mkdir offline
-  Mount-WindowsImage -Path .\offline\ -ImagePath .\wim\install.wim -Name $args[1]
-} -ArgumentList $jobUnpackWim, $version
+# $thumbDrive = $thumbDriveLetter+':\'
+# #  mount iso
+# $isoDriveLetter = (Mount-DiskImage $isoFile -PassThru | Get-Volume).DriveLetter
 
 
-#import drivers
-$jobAddDrivers = Start-Job -ScriptBlock {
-  $args[0] | Wait-Job
-  $args[1] | Wait-Job
-  Add-WindowsDriver -Path .\offline -Driver .\drivers -Recurse
-} -ArgumentList $jobExportDrivers, $jobMountImage
+# #TODO check letter automtaically
+# If ($isoDriveLetter -eq ""){echo couldnt mount image; exit}
+# If ($isoDriveLetter -eq $null){echo couldnt mount image; exit}
+
+# #  move to temp dir
+# $tmpdir = New-TemporaryFile | %{ rm $_ ; mkdir $_ }
+# cd $tmpdir
 
 
-# remove YourPhone app from mounted
-$jobCustomizeMount = Start-Job -ScriptBlock {
-  $args[0] | Wait-Job
+
+
+
+# #copy iso to stick
+# $jobFileCopy = Start-Job -ScriptBlock {
+#   Format-Volume -DriveLetter $args[0] -FileSystem FAT32 -NewFileSystemLabel "Windows10"
+#   robocopy $args[1] $args[2] /S /XF install.esd
+# } -ArgumentList $thumbDriveLetter, $isoDriveLetter':\', $thumbDrive
+
+# #export drivers
+# $jobExportDrivers = Start-Job -ScriptBlock {
+#   cd $args[0]
+#   mkdir drivers
+#   Export-WindowsDriver -Online -Destination .\drivers
+# } -ArgumentList $pwd
+
+# #iso/esd -> wim
+# $jobUnpackEsd = Start-Job -ScriptBlock {
+#   cd $args[0]
+#   mkdir wim
+#   Dism.exe /Export-Image $args[1], $args[2] /DestinationImageFile:.\wim\install.wim  /Compress:Max /CheckIntegrity
+# } -ArgumentList $pwd, /SourceImageFile:$isoDriveLetter":\sources\install.esd", /SourceName:$version
+
+
+# #TODO wim mount
+# $jobMountImage = Start-Job -ScriptBlock {
+#   cd $args[0]
+#   $args[1] | Wait-Job 
+#   mkdir offline
+#   Mount-WindowsImage -Path .\offline\ -ImagePath .\wim\install.wim -Name $args[2]
+# } -ArgumentList $pwd, $jobUnpackEsd, $version
+
+
+# #import drivers
+# $jobAddDrivers = Start-Job -ScriptBlock {
+#   cd $args[0]
+#   $args[1] | Wait-Job
+#   $args[2] | Wait-Job
+#   Add-WindowsDriver -Path .\offline -Driver .\drivers -Recurse
+# } -ArgumentList $pwd, $jobExportDrivers, $jobMountImage
+
+
+# # remove YourPhone app from mounted
+# $jobCustomizeMount = Start-Job -ScriptBlock {
+#   cd $args[0]
+#   $args[1] | Wait-Job
   
-  Remove-AppxProvisionedPackage -PackageName Microsoft.YourPhone -Path .\offline
-  Enable-WindowsOptionalFeature -Path .\offline -FeatureName "NetFx3" -All
+#   Remove-AppxProvisionedPackage -PackageName Microsoft.YourPhone -Path .\offline
+#   Enable-WindowsOptionalFeature -Path .\offline -FeatureName "NetFx3" -All
   
-  # set product key
-  $productKey = (Get-WmiObject -query 'select * from SoftwareLicensingService').OA3xOriginalProductKey
-  Set-WindowsProductKey -Path .\offline -ProductKey $productKey
+#   # set product key
+#   $productKey = (Get-WmiObject -query 'select * from SoftwareLicensingService').OA3xOriginalProductKey
+#   Set-WindowsProductKey -Path .\offline -ProductKey $productKey
   
-} -ArgumentList $jobMountImage
+# } -ArgumentList $pwd, $jobMountImage
 
 
-$jobUnMountImage = Start-Job -ScriptBlock {
-  $args[0] | Wait-Job
-  $args[1] | Wait-Job
-  Dismount-WindowsImage -Path .\offline -Save
-  copy .\wim\install.wim $args[2]
-} -ArgumentList $jobCustomizeMount, $jobAddDrivers, $thumbDrive'sources\install.wim'
+# $jobUnMountImage = Start-Job -ScriptBlock {
+#   cd $args[0]
+#   $args[1] | Wait-Job
+#   $args[2] | Wait-Job
+#   Dismount-WindowsImage -Path .\offline -Save
+#   copy .\wim\install.wim $args[3]
+# } -ArgumentList $pwd, $jobCustomizeMount, $jobAddDrivers, $thumbDrive'sources\install.wim'
 
-echo waiting for tasks to finish
-$jobUnMountImage | Wait-Job
-echo done
-pause
+# echo waiting for tasks to finish
+# $jobUnMountImage | Wait-Job
+# echo done
+# pause
